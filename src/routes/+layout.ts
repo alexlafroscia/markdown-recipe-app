ad1c5ab77a26ffb0b32d842b3b9e4ab0fe7cda5b
Alex LaFroscia <alex@lafroscia.com>
2025-01-30 10:24:58 -0500

    static serving improvements

---
+++ src/routes/+layout.ts
@@ -0,0 +1,6 @@
+// Pre-render all pages
+export const prerender = true;
+
+// Always enforce a trailing slash in URLs
+// This seems to match the behavior of the Docker image
+export const trailingSlash = 'always';
