df1039e44b37f3af7e4b7ab3c5a86c9bb835f8ee
Alex LaFroscia <alex@lafroscia.com>
2025-02-02 14:46:14 -0500

    allow `Button` to accept any `ClassValue`

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
-	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import type { ClassValue, HTMLButtonAttributes } from 'svelte/elements';
 
 	interface Props extends HTMLButtonAttributes {
+		class?: ClassValue;
 		children: Snippet;
 	}
 
 	let { children, class: classNames, ...rest }: Props = $props();
 </script>
 
-<button class="flex gap-2 rounded-sm p-2 transition-colors hover:shadow-xs {classNames}" {...rest}>
+<button
+	class={['flex gap-2 rounded-sm p-2 transition-colors hover:shadow-xs', classNames]}
+	{...rest}
+>
 	{@render children()}
 </button>
